dc8501c8ebbf08fcd361842719ca0f24dc317537
kwanyeol <djm030@naver.com>
2024-03-12 18:51:42 +0900

    update _ project

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,22 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask.cli import with_appcontext
 import os
 import click
 from .database import db
-from .models import Question  # Question 모델 임포트
+from .models import Question, Admin ,Participant  # Question 모델 임포트
+from werkzeug.security import generate_password_hash
+from datetime import datetime, timedelta
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'oz_coding_secret'
 
     # 데이터베이스 파일 경로 설정 및 앱 설정
     basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    dbfile = os.path.join(basedir, 'db.sqlite')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    dbfile = os.path.join(basedir, "db.sqlite")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + dbfile
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # 데이터베이스 및 마이그레이션 초기화
     db.init_app(app)
@@ -22,7 +24,10 @@ def create_app():
 
     # 라우트(블루프린트) 등록
     from .routes import main as main_blueprint
+    from .routes import admin as admin_blueprint
+
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(admin_blueprint)
 
     # 초기화 명령어 정의
     def add_initial_questions():
@@ -33,20 +38,43 @@ def create_app():
             "프로젝트를 진행해보신적 있으신가요?",
             "개발자로 일한 경력이 있으신가요?",
         ]
+        yesterday = datetime.utcnow() - timedelta(days=1)  # 어제 날짜 계산
+
+        # 관리자 계정 추가 로직, 비밀번호 해시 처리 적용
+        existing_admin = Admin.query.filter_by(username="admin").first()
+        if not existing_admin:
+            hashed_password = generate_password_hash("0000")  # 비밀번호를 해시 처리
+            new_admin = Admin(username="admin", password=hashed_password)
+            db.session.add(new_admin)
+
+
+
+        participants_without_created_at = Participant.query.filter(Participant.created_at == None).all()
+        
+        for participant in participants_without_created_at:
+            participant.created_at = yesterday
+        
+
 
         for question_content in initial_questions:
-            existing_question = Question.query.filter_by(content=question_content).first()
+            existing_question = Question.query.filter_by(
+                content=question_content
+            ).first()
             if not existing_question:
                 new_question = Question(content=question_content)
                 db.session.add(new_question)
+        questions = Question.query.all()
+        for question in questions:
+            question.order_num = question.id
+            question.is_active = True  # 모든 질문을 활성화 상태로 설정
         db.session.commit()
 
-    @click.command('init-db')
+    @click.command("init-db")
     @with_appcontext
     def init_db_command():
         db.create_all()
         add_initial_questions()
-        click.echo('Initialized the database.')
+        click.echo("Initialized the database.")
 
     app.cli.add_command(init_db_command)
 
